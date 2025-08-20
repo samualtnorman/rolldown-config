eb70092ad06939ee1e73559d99a1d33b374b2c64
Samual Norman <me@samual.uk>
2025-08-20 16:43:45 +0100

    self host this also means switching from Rollup to Rolldown to build this package

--- scripts/jsr.sh
+++ scripts/jsr.sh
@@ -2,7 +2,7 @@
 set -ex
 rm dist --recursive --force
 mkdir dist --parents
-./rollup.config.js --configJsr
+JSR=1 ./rolldown.config.js
 scripts/emit-dts.sh
 cp LICENSE dist
 cat readme.md | scripts/prepend-readme.js dist/default.d.ts
