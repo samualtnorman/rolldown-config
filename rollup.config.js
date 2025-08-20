9a6b6c8de93f125ee444ae325937dd68aaed83c5
Samual Norman <me@samual.uk>
2025-08-20 16:29:47 +0100

    add `@ts-self-types` comment to top of generated `default.js` so jsr can find the `.d.ts` file

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,12 @@
 #!node_modules/.bin/rollup --config
+import Path from "path"
 import { rollupConfig } from "./node_modules/@samual/rollup-config/index.js"
 
-export default rollupConfig()
+export default (/** @type {Record<string, unknown>} */ args) => rollupConfig(
+	args.configJsr
+		? {
+			rollupOptions: { output: { banner: chunk => `// @ts-self-types="./${Path.basename(chunk.name)}.d.ts"` } },
+			terserOptions: { format: { comments: /@ts-self-types/ } }
+		}
+		: undefined
+)
