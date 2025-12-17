f7ec7116ac3838ed7049fe781247da8c06947a46
Samual Norman <me@samual.uk>
2025-12-17 11:23:45 +0000

    update `emit-jsr-json.js` and add prod deps to be converted to JSR deps

--- scripts/emit-jsr-json.js
+++ scripts/emit-jsr-json.js
@@ -2,17 +2,38 @@
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson from "../package.json" with { type: "json" }
 
+if (!process.env.FULL_ERROR) {
+	process.on(`uncaughtException`, error => {
+		console.error(error.message)
+		process.exit(1)
+	})
+}
+
+/** @type {Record<string, string>} */ const ConvertToJsr = {
+	"@samual/assert": "@sn/assert",
+	"@samual/types": "@samual/types",
+	"babel-plugin-here": "@sn/babel-plugin-here"
+}
+
 const { name, version, license, dependencies } = packageJson
 
 makeDirectorySync("dist", { recursive: true })
 
-const imports =
-	Object.fromEntries(Object.entries(dependencies).map(([ name, version ],) => [ name, `npm:${name}@${version}` ]))
-
-writeFileSync("dist/jsr.json", JSON.stringify(
-	{ name, version, license, exports: { ".": "./default.js" }, imports },
-	undefined,
-	"\t"
+const imports = Object.fromEntries(Object.entries(dependencies).map(
+	([ name, version ]) => [
+		name.startsWith(`@types/`) ?
+			name.includes(`__`) ?
+				`@${name.slice(7, name.indexOf(`__`))}/${name.slice(name.indexOf(`__`) + 2)}`
+			: name.slice(7)
+		: name,
+		`${name in ConvertToJsr ? `jsr:${ConvertToJsr[name]}` : `npm:${name}`}@${version}`
+	]
 ))
 
-process.exit()
+writeFileSync("dist/jsr.json", JSON.stringify({
+	name,
+	version,
+	license,
+	exports: { ".": "./default.js" },
+	imports
+}, undefined, "\t"))
