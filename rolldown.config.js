a648134efc7c3e114b159b5d8623d4d562390c75
Samual Norman <me@samual.uk>
2025-12-17 11:30:38 +0000

    rename `@samual/rolldown-config` self dep to `@samual/previous-rolldown-config` so its name does not clash

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,11 +1,11 @@
 #!node_modules/.bin/rolldown --config
+import { rolldownConfig } from "@samual/previous-rolldown-config"
 import Path from "path"
-import { rolldownConfig } from "./node_modules/@samual/rolldown-config/default.js"
 
 export default rolldownConfig(
 	process.env.JSR
 		? {
-			rolldownOptions: { output: { banner: (/** @type {import("rolldown").AddonFunction} */ chunk) => `// @ts-self-types="./${Path.basename(chunk.name)}.d.ts"` } },
+			rolldownOptions: { output: { banner: chunk => `// @ts-self-types="./${Path.basename(chunk.name)}.d.ts"` } },
 			terserOptions: { format: { comments: /@ts-self-types/ } }
 		}
 		: undefined
