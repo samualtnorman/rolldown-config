ddcc0c79ed1c4e05531460f6bc7bfdd46937e81e
Samual Norman <me@samual.uk>
2026-03-17 16:32:08 +0000

    update `shell.nix`

--- shell.nix
+++ shell.nix
@@ -4,8 +4,8 @@ let fetchNixpkgs =
   { rev, sha256 ? "" }: import (fetchFromGitHub { owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {}; in
 
 let inherit (fetchNixpkgs {
-  rev = "71caefce12ba78d84fe618cf61644dce01cf3a96"; # 24.11 2025/03/11
-  sha256 = "yf3iYLGbGVlIthlQIk5/4/EQDZNNEmuqKZkQssMljuw=";
+  rev = "48652e9d5aea46e555b3df87354280d4f29cd3a3"; # 25.11 2026/03/17
+  sha256 = "xB30bbAp0e7ogSEYyc126mAJMt4FRFh8wtm6ADE1xuM=";
 }) nodejs_22 pnpm_10; in
 
 mkShellNoCC { packages = [ cacert git nodejs_22 pnpm_10 ]; }
