4ea4de9cf38c9c633f37dd551b8909bd20e39417
Samual Norman <me@samual.uk>
2026-04-17 11:41:32 +0100

    make `version-dev.js` check if version actually changed

--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 import { expect } from "@samual/assert"
 import { spawnSync } from "child_process"
+import { readFileSync } from "fs"
 import * as Semver from "semver"
 import packageJson from "../package.json" with { type: "json" }
 
 const hash = spawnSync("git", [ "rev-parse", "--short", "HEAD" ], { encoding: "utf8" }).stdout.trim()
 
 spawnSync("pnpm", [ "version", `${expect(Semver.inc(packageJson.version, "patch"))}-${hash}` ], { stdio: "inherit" })
+
+const newVersion = /** @type {Record<string, unknown> | null} */
+	(JSON.parse(readFileSync(`package.json`, { encoding: `utf8` })))?.version
+
+if (packageJson.version == newVersion) {
+	console.error(`Version didn't change`)
+	process.exit(1)
+}
+
 process.exit()
